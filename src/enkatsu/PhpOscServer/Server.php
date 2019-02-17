612aa40d42e3784101b0bc53fe96942fa1a57173
enkatsu <endkty0509@gmail.com>
2019-02-17 18:37:50 +0900

    :hammer: constructor

--- src/enkatsu/PhpOscServer/Server.php
+++ src/enkatsu/PhpOscServer/Server.php
@@ -6,14 +6,18 @@ use Enkatsu\PhpOscParser\Parser;
 class Server {
   private $parser;
   private $BUFFER_SIZE = 8192;
+  private $host;
+  private $port;
   
-  function __construct() {
+  function __construct($host, $port) {
     $this->parser = new Parser();
+    $this->host = $host;
+    $this->port = $port;
   }
   
-  public function recieve($host, $port) {
+  public function recieve() {
     $socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
-    socket_bind($socket, $host, $port);
+    socket_bind($socket, $this->host, $this->port);
     socket_recv($socket, $buf, $BUFFER_SIZE, 0);
     $this->parser->parse($buf);
     return $this->parser->messages;
