748828a1de9e2b9dc43fcac13778a2325640e1f9
enkatsu <endkty0509@gmail.com>
2019-02-17 18:07:14 +0900

    init

---
+++ src/enkatsu/PhpOscServer/Server.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Enkatsu\PhpOscServer;
+use Enkatsu\PhpOscParser\Parser;
+
+class PhpOscServer {
+  private $parser;
+  private $BUFFER_SIZE = 8192;
+  
+  function __construct() {
+    $this->parser = new Parser();
+  }
+  
+  public function recieve($host, $port) {
+    $socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+    socket_bind($socket, $host, $port);
+    socket_recv($socket, $buf, $BUFFER_SIZE, 0);
+    $this->parser->parse($buf);
+    return $this->parser->messages;
+  }
+}
