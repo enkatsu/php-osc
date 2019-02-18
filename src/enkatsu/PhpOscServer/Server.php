66c80c6df6f79e264a5335ee18b6e36b59a931ac
KatsuyaEndoh <k.endo@s-arcana.co.jp>
2019-02-18 13:17:52 +0900

    :hammer: [refactor]

--- src/enkatsu/PhpOscServer/Server.php
+++ src/enkatsu/PhpOscServer/Server.php
@@ -9,7 +9,7 @@ class Server {
   private $socket;
   private $host;
   private $port;
-  
+
   function __construct($host, $port) {
     $this->parser = new Parser();
     $this->host = $host;
@@ -17,7 +17,7 @@ class Server {
     $this->socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
     socket_bind($this->socket, $this->host, $this->port);
   }
-  
+
   public function recieve() {
     // socket_recv($this->socket, $buf, self::$BUFFER_SIZE, 0);
     $buf = socket_read($this->socket, self::$BUFFER_SIZE, PHP_BINARY_READ);
@@ -25,7 +25,6 @@ class Server {
     if(is_null($buf)) return;
     $pos = 0;
     $buf = collect(str_split(bin2hex($buf), 8));
-    $this->parser->parse($buf, $pos, $buf->count() - 1);
-    return $this->parser->flush();
+    return $this->parser->parse($buf, $pos, $buf->count() - 1);
   }
 }
