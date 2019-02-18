868104691d73408459219eae0b1cf2ca514c01d4
KatsuyaEndoh <k.endo@s-arcana.co.jp>
2019-02-18 14:01:09 +0900

    :hammer: [refactor] & :bento: [add] Exception

--- src/enkatsu/PhpOscServer/Server.php
+++ src/enkatsu/PhpOscServer/Server.php
@@ -1,16 +1,21 @@
 <?php
 
 namespace Enkatsu\PhpOscServer;
-use Enkatsu\PhpOscParser\Parser;
 
-class Server {
+use Tightenco\Collect\Support\Collection;
+use Enkatsu\PhpOscParser\Parser;
+use Enkatsu\PhpOscParser\Bundle;
+
+class Server
+{
   static private $BUFFER_SIZE = 8192;
   private $parser;
   private $socket;
   private $host;
   private $port;
 
-  function __construct($host, $port) {
+  function __construct(string $host, int $port)
+  {
     $this->parser = new Parser();
     $this->host = $host;
     $this->port = $port;
@@ -18,11 +23,12 @@ class Server {
     socket_bind($this->socket, $this->host, $this->port);
   }
 
-  public function recieve() {
+  public function recieve(): ?Bundle
+  {
     // socket_recv($this->socket, $buf, self::$BUFFER_SIZE, 0);
     $buf = socket_read($this->socket, self::$BUFFER_SIZE, PHP_BINARY_READ);
     flush();
-    if(is_null($buf)) return;
+    if(is_null($buf)) return null;
     $pos = 0;
     $buf = collect(str_split(bin2hex($buf), 8));
     return $this->parser->parse($buf, $pos, $buf->count() - 1);
